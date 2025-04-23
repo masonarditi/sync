43bfb603805f37864ded2c194155321e682f2a68
Mason Arditi <masonarditi@berkeley.edu>
2025-04-22 22:50:00 -0700

    Initial Commit

---
+++ veer-swift/veer_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  veer_swiftApp.swift
+//  veer-swift
+//
+//  Created by Mason on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct veer_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ veer-swiftTests/veer_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  veer_swiftTests.swift
+//  veer-swiftTests
+//
+//  Created by Mason on 4/22/25.
+//
+
+import Testing
+@testable import veer_swift
+
+struct veer_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
